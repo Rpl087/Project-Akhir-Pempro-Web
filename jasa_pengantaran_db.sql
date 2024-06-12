9af6ad356e6817bd666540d66b9f4f4e122e5ac1
Christian Andreas Roeroe <160499858+Reasonly40@users.noreply.github.com>
2024-06-12 12:45:50 +0800

    Add files via upload

--- jasa_pengantaran_db.sql
+++ jasa_pengantaran_db.sql
@@ -1,7 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3308
+-- Generation Time: Jun 07, 2024 at 01:02 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jasa_pengantaran_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipments`
+--
 
 CREATE TABLE `shipments` (
   `id` int(11) NOT NULL,
@@ -16,6 +40,9 @@ CREATE TABLE `shipments` (
   `service` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `shipments`
+--
 
 INSERT INTO `shipments` (`id`, `user_id`, `tracking_number`, `status`, `shipment_date`, `sender`, `receiver`, `sender_address`, `receiver_address`, `service`) VALUES
 (1, 3, 'TRACK123', 'Shipped', '2024-06-01', 'Christian Roeroe', 'Romal Lengkong', 'Teling', 'Tikala', 'Express'),
@@ -23,6 +50,11 @@ INSERT INTO `shipments` (`id`, `user_id`, `tracking_number`, `status`, `shipment
 (3, 5, 'TRACK789', 'Delivered', '2024-06-03', 'Feykha Koem', 'Romal Lengkong', 'Kampus Bahu', 'Tikala', 'Biasa'),
 (4, 6, 'TRACK101', 'Pending', '2024-06-04', 'Marvell Palenewen', 'Christian Roeroe', 'Kotamobagu', 'Teling', 'Express');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -34,6 +66,10 @@ CREATE TABLE `users` (
   `user_type` enum('admin','member') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `users`
+--
+
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `phone`, `user_type`) VALUES
 (1, 'admin1', 'adminpassword1', 'Durant', 'admin1@example.com', '08991111222', 'admin'),
 (2, 'admin2', 'adminpassword2', 'Admin Two', 'admin2@example.com', '2222222222', 'admin'),
@@ -42,24 +78,52 @@ INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `phone`, `us
 (5, 'fkoem', 'memberpassword3', 'Feykha Koem', 'fkoem@example.com', '5555555555', 'member'),
 (6, 'mpalenewen', 'memberpassword4', 'Marvell Palenewen', 'mpalenewen@example.com', '6666666666', 'member');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shipments`
+--
 ALTER TABLE `shipments`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `tracking_number` (`tracking_number`),
   ADD KEY `user_id` (`user_id`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `shipments`
+--
 ALTER TABLE `shipments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
-
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `shipments`
+--
 ALTER TABLE `shipments`
   ADD CONSTRAINT `shipments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
